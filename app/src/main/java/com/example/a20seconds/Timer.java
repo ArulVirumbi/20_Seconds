b0afde8295aed6440f23090b9ac47c350719a995
Sudarshan S <79958765+sudarshan-hub@users.noreply.github.com>
2023-02-16 16:06:05 +0530

    Update Timer.java

--- app/src/main/java/com/example/a20seconds/Timer.java
+++ app/src/main/java/com/example/a20seconds/Timer.java
@@ -15,7 +15,7 @@ public class Timer{
             handler.postDelayed(this, 55000);  // total delay time (windowON + windowOFF)
         }
     };
-    /**
+   /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
      *
      * @param name Used to name the worker thread, important only for debugging.
